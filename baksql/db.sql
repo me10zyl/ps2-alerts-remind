93e2babb1f5ae12dcb628120c635a9fc0fa82bde
astashz <me10zyl@qq.com>
2022-03-17 14:35:41 +0800

    sqlite update

--- baksql/db.sql
+++ baksql/db.sql
@@ -3,7 +3,8 @@ create table subscribe_user
 (
     email            varchar(100) not null,
     server           varchar(20)  not null,
-    alert_start_time datetime
+    alert_start_time datetime,
+    qq               varchar(20)
 );
 
 create unique index subscribe_users_email_server_uindex
