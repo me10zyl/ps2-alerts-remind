9bcabea01362257aa956fb754101ccb4d6494c39
astashz <me10zyl@qq.com>
2022-03-25 11:49:05 +0800

    QQ好友申请.

--- baksql/db.sql
+++ baksql/db.sql
@@ -1,14 +1,10 @@
 -- auto-generated definition
 create table subscribe_user
 (
-    email            varchar(100) not null,
-    server           varchar(20)  not null,
+    email            varchar(100),
+    server           varchar(20) not null,
     alert_start_time datetime,
     qq               varchar(20),
-    is_send_mail     integer default 1,
     is_qq_group      integer default 0
 );
-
-create unique index subscribe_users_email_server_uindex
-    on subscribe_user (email, server);
 
